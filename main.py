620b9ab2d9f04a4145ac55237ef088dba10d492d
matiasilva <mw781@srcf.net>
2022-01-20 15:03:41 +0000

    Refactor and implement API functionality

--- main.py
+++ main.py
@@ -1,23 +1,44 @@
+#!/usr/bin/env python
+
 # timelapse - a simple script that captures sunrises and sunsets
+# TODO: take daylight saving time into account
 
 import time
-from datetime import timedelta, datetime
+import datetime as dt
 import os
 import subprocess
+import yaml
+import requests
 
-SUNRISE = "07:50"
-SUNSET = "16:25"
-OUTPUT_FOLDER = "output"
-IMAGE_STRING = "image-{}"
-TIMELAPSE_INTERVAL = 3
+API_ENDPOINT = "https://api.sunrise-sunset.org/json?lat={}&lng={}&formatted=0"
+config = {}
+
+IMAGE_STRING = "-o image-{}"
 
 
 def main():
+    # read config
+    with open("config.yaml", "r") as f:
+        try:
+            config = yaml.safe_load(f)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    # fetch data from API
+    # TODO: optimize so we don't check every day
+    coords = (config.get("latitude"), config.get("longitude"))
+    times = get_times(API_ENDPOINT, coords)
+    if times is None:
+        sunrise = time_to_dt(config.get("sunrise"))
+        sunset = time_to_dt(config.get("sunset"))
+        times = (sunrise, sunset)
+
     # time check
-    padding = timedelta(hours=1, minutes=30)
+    padding = dt.timedelta(hours=1, minutes=30)
+    TIMELAPSE_INTERVAL = config.get("timelapse_interval", 3)
     count = round((padding * 2).total_seconds() / TIMELAPSE_INTERVAL)
-    is_sunrise = has_triggered(SUNRISE, padding)
-    is_sunset = has_triggered(SUNSET, padding)
+    is_sunrise = has_triggered(times[0], padding)
+    is_sunset = has_triggered(times[1], padding)
     if is_sunrise:
         start_timelapse("sunrise", count)
     elif is_sunset:
@@ -25,37 +46,64 @@ def main():
     return
 
 
-def has_triggered(mid, pad):
+def time_to_dt(t_raw):
+    now = dt.datetime.now()
+    t = dt.time.fromisoformat(t_raw)
+    return now.replace(hour=t.hour, minute=t.minute)
+
+
+def get_times(url, coords):
+    (lat, long) = coords
+    endpoint = url.format(lat, long)
+    data = requests.get(endpoint)
+    is_valid = data.status_code == requests.codes.ok
+    data_json = data.json()
+    if data_json.get("status") == "OK" and is_valid:
+        results = data_json.get("results")
+        sunrise = format_dt(results.get("sunrise"))
+        sunset = format_dt(results.get("sunset"))
+        return (sunrise, sunset)
+    return None
+
+
+def format_dt(raw_t):
+    # drop the unnecessary precision of the second's place, use naive dt
+    return dt.datetime.fromisoformat(raw_t).replace(second=0, tzinfo=None)
+
+
+def has_triggered(mid_dt, pad):
     """
     checks if the current time is within a range specified by a midpoint and equal padding around the midpoint
 
-    mid - ISO-formatted time
+    mid - datetime object
     pad - padding around the midpoint, filming triggers after (mid - pad) and stops after (mid + pad)
     """
-    now = datetime.now()
-    mid_t = time.fromisoformat(mid)
-    mid_dt = now.replace(hour=mid_t.hour, minute=mid_t.minute)
+    now = dt.datetime.now()
     trigger_time = mid_dt - pad
     end_time = mid_dt + pad
     return now > trigger_time and now < end_time
 
 
 def start_timelapse(type, count):
+    # constants
+    TIMELAPSE_INTERVAL = config.get("timelapse_interval", 3)
+    OUTPUT_FOLDER = config.get("output_folder", "output")
+
     # create folder structure
-    now = datetime.now()
+    now = dt.datetime.now()
     file_path = f"{OUTPUT_FOLDER}/{now.year}/{now.month}/{now.day}/f{type}"
     os.makedirs(file_path, exist_ok=True)
 
     for i in range(count):
         # take photo
-        subprocess.run(['libcamera-jpeg', '-o ' +
-                       IMAGE_STRING.format(i), '-n', '-q 100'])
+        subprocess.run(
+            ['libcamera-jpeg', IMAGE_STRING.format(i), '-n', '-q 100'])
         # rest for a bit
         time.sleep(TIMELAPSE_INTERVAL)
 
     # when done, stitch the photos together
-    subprocess.run(['ffmpeg', '-framerate 30', '-pattern_type glob' '-i "{file_path}/*.JPG"',
-                   '-s:v 1440x1080', '-c:v libx264', '-crf 17', '-pix_fmt yuv420p', f'output.mp4'])
+    subprocess.run(['ffmpeg', '-framerate 30', '-pattern_type glob',
+                   '-i "{file_path}/*.JPG"', '-s:v 1440x1080', '-c:v libx264', '-crf 17', '-pix_fmt yuv420p', f'output.mp4'])
 
 
 if __name__ == "__main__":
